c3f3420452231497f10b7eafe1e1c60ecbe3c457
hjyoo <wlak56@naver.com>
2026-01-20 17:58:50 +0900

    django orm

---
+++ source/12_django/ch03_orm/student/urls.py
@@ -0,0 +1,13 @@
+# student 앱 아래의 urls.py
+# student/          - name = "student:list"
+# student/get/1     - name = "student:get"
+# sydent/delete/1   - name = "student:delete"
+from django.urls import path, register_converter
+from . import views
+from .converters import MyConverter
+register_converter(MyConverter, "ddd")
+app_name = "student"
+urlpatterns = [
+    path("", views.list, name="list"),
+    path("get/<ddd:id>", views.get, name="get"),
+]
--- source/12_django/ch03_orm/student/views.py
+++ source/12_django/ch03_orm/student/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from student.models import Student
 # Create your views here.
+def list(request):
+    students = Student.objects.all() # select * from student_student
+    # return HttpResponse(students)
+    return render(request, "student/list.html", {"students":students})
+def get(request, id:int):
+    student = Student.objects.get(pk=id)
+    return render(request, "student/get.html", {"student":student})
